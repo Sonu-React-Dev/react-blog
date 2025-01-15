6975e83758bf0b92aadebbed855d88717492d6fe
ImSonuEduMitram <sonugupta6746@gmail.com>
2025-01-15 14:02:23 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 
 const App = () => {
   return (
-    <div className='text-6xl text-red-950 '>App blog</div>
-  )
-}
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dash" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
